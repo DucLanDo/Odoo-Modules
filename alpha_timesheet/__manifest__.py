cea4a9df29aadfab89e0f00ce21c7f67e1055b51
Duc Lan Do <ducii_lan@hotmail.de>
2026-03-10 15:33:00 +0100

    Initial commit

---
+++ alpha_timesheet/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Alpha Timesheet",
+    "version": "19.0.1.0.0",
+    "summary": "Simple custom timesheet for employees",
+    "category": "Services",
+    "author": "alphaIT",
+    "license": "LGPL-3",
+    "depends": ["base", "contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/alpha_timesheet_rules.xml",
+        "views/alpha_timesheet_views.xml",
+        "views/alpha_timesheet_menus.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ alpha_timesheet/models/timesheet_entry.py
@@ -0,0 +1,90 @@
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class AlphaTimesheetEntry(models.Model):
+    _name = "alpha.timesheet.entry"
+    _description = "Alpha Timesheet Entry"
+    _order = "date desc, time_from desc"
+
+    name = fields.Char(string="Description", required=True)
+
+    date = fields.Date(
+        string="Date",
+        required=True,
+        default=fields.Date.context_today,
+    )
+
+    partner_id = fields.Many2one(
+        "res.partner",
+        string="Customer",
+        required=True,
+        domain=[("is_company", "=", True)],
+    )
+
+    partner_street = fields.Char(
+        string="Street",
+        related="partner_id.street",
+        readonly=True,
+    )
+    partner_zip = fields.Char(
+        string="ZIP",
+        related="partner_id.zip",
+        readonly=True,
+    )
+    partner_city = fields.Char(
+        string="City",
+        related="partner_id.city",
+        readonly=True,
+    )
+    partner_email = fields.Char(
+        string="E-Mail",
+        related="partner_id.email",
+        readonly=True,
+    )
+    partner_phone = fields.Char(
+        string="Phone",
+        related="partner_id.phone",
+        readonly=True,
+    )
+
+    employee_user_id = fields.Many2one(
+        "res.users",
+        string="Created By",
+        required=True,
+        default=lambda self: self.env.user,
+        readonly=True,
+    )
+
+    time_from = fields.Float(
+        string="Time From",
+        required=True,
+        help="Example: 08:30",
+    )
+
+    time_to = fields.Float(
+        string="Time To",
+        required=True,
+        help="Example: 16:30",
+    )
+
+    duration_minutes = fields.Integer(
+        string="Minutes",
+        compute="_compute_duration_minutes",
+        store=True,
+        readonly=True,
+    )
+
+    @api.depends("time_from", "time_to")
+    def _compute_duration_minutes(self):
+        for rec in self:
+            if rec.time_from is not False and rec.time_to is not False and rec.time_to >= rec.time_from:
+                rec.duration_minutes = int(round((rec.time_to - rec.time_from) * 60))
+            else:
+                rec.duration_minutes = 0
+
+    @api.constrains("time_from", "time_to")
+    def _check_times(self):
+        for rec in self:
+            if rec.time_to < rec.time_from:
+                raise ValidationError("Time To must be later than Time From.")
